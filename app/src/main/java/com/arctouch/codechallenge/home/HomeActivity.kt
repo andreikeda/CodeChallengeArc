f72a627f01ee123dd688a08b787b9863e83a41c4
Andre Ikeda <andre.ikeda.lima@everis.com>
2018-09-26 11:31:56 -0300

    completed customization to viper architecture

--- app/src/main/java/com/arctouch/codechallenge/home/HomeActivity.kt
+++ app/src/main/java/com/arctouch/codechallenge/home/HomeActivity.kt
@@ -3,29 +3,48 @@ package com.arctouch.codechallenge.home
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
+import android.widget.Toast
 import com.arctouch.codechallenge.R
 import com.arctouch.codechallenge.api.TmdbApi
 import com.arctouch.codechallenge.base.BaseActivity
 import com.arctouch.codechallenge.data.Cache
+import com.arctouch.codechallenge.model.Movie
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.home_activity.*
 
-class HomeActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity(), HomeModule.View {
+
+    private var presenter: HomeModule.Presenter? = null
+
+    override fun hideLoading() {
+        progressBar.visibility = View.GONE
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.home_activity)
 
-        api.upcomingMovies(TmdbApi.API_KEY, TmdbApi.DEFAULT_LANGUAGE, 1, TmdbApi.DEFAULT_REGION)
-            .subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe {
-                val moviesWithGenres = it.results.map { movie ->
-                    movie.copy(genres = Cache.genres.filter { movie.genreIds?.contains(it.id) == true })
-                }
-                recyclerView.adapter = HomeAdapter(moviesWithGenres)
-                progressBar.visibility = View.GONE
-            }
+        presenter = HomePresenter(this)
+        presenter?.callMoviesApi(1)
+    }
+
+    override fun onDestroy() {
+        presenter?.unregister()
+        presenter = null
+
+        super.onDestroy()
+    }
+
+    override fun setAdapter(movies: List<Movie>) {
+        recyclerView.adapter = HomeAdapter(movies)
+    }
+
+    override fun showError(errorMessage: String) {
+        Toast.makeText(this, errorMessage, Toast.LENGTH_LONG).show()
+    }
+
+    override fun showLoading() {
+        progressBar.visibility = View.VISIBLE
     }
 }
