084285ff251babe50f474ef0fede4499f663035c
Andre Ikeda <andre.ikeda.lima@everis.com>
2018-09-26 11:00:36 -0300

    created home module interface using viper architecture

---
+++ app/src/main/java/com/arctouch/codechallenge/home/HomeModule.kt
@@ -0,0 +1,35 @@
+package com.arctouch.codechallenge.home
+
+import com.arctouch.codechallenge.model.Movie
+import com.arctouch.codechallenge.model.UpcomingMoviesResponse
+
+interface HomeModule {
+
+    interface Interactor {
+        fun loadMovies(page: Int)
+        fun unregister()
+    }
+
+    interface InteractorOutput {
+        fun loadedMoviesError(errorMessage: String)
+        fun loadedMoviesSuccess(response: UpcomingMoviesResponse)
+    }
+
+    interface Presenter {
+        fun callMoviesApi(page: Int)
+        fun unregister()
+    }
+
+    interface Router {
+        fun goToDetailScreen(movie: Movie)
+        fun unregister()
+    }
+
+    interface View {
+        fun hideLoading()
+        fun setAdapter(movies: List<Movie>)
+        fun showError(errorMessage: String)
+        fun showLoading()
+    }
+
+}
